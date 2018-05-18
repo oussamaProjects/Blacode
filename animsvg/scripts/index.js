a3c30f996bc382e0f3308bbdf121b40619d9c52b
Badro0 <0.badro65@gmail.com>
2018-05-18 15:15:11 +0000

    svg animation

---
+++ animsvg/scripts/index.js
@@ -0,0 +1,26 @@
+lottie.loadAnimation({
+  container: bm, // the dom element that will contain the animation
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'pixi.json' // the path to the animation json
+});
+
+// var bm = document.getElementById("bm");
+// var animPhone = lottie.loadAnimation({
+// container: bm,
+// renderer: "svg",
+// loop: true,
+// autoplay: false,
+// prerender: true,
+// path: "voyage.json"
+// });
+//
+//
+// bm.addEventListener("mouseenter", function () {
+// animPhone.play();
+// });
+//
+// bm.addEventListener("mouseleave", function () {
+// animPhone.stop();
+// });
